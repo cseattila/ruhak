50b59ee8a3cfc5c273f77d3a9409b87d87f7233d
cseattila <cseattila@lak.csengeri.ml>
2020-08-07 20:15:31 +0200

    git add php engine meg homonai dolgok

---
+++ php_engine/engine.php
@@ -0,0 +1,68 @@
+<?php
+   $conn=mysqli_connect('localhost','majus','majus12345','majus');
+if ($conn->connect_error) {
+				die("Dase connection failed: " . $dbconnect->connect_error);
+	}
+function get_homonaiDaTa() {
+ 	global $conn;
+		
+	$sql = "SELECT * FROM homonai";
+	$sth = $conn->query($sql);
+
+
+	$rows = array();
+	while($r = mysqli_fetch_assoc($sth)) {
+		$rows[] = $r;
+	}
+	print json_encode($rows);
+}
+
+function get_file() {
+ 	global $conn;
+	
+	$sql = "SELECT content,mime_type FROM virtual_file where id=?";
+    $stmt = mysqli_prepare( $conn,$sql);
+	
+	mysqli_stmt_bind_param($stmt,'i',$id);
+    $id=	$_GET['id'];
+
+
+    mysqli_stmt_execute($stmt);
+  
+    mysqli_stmt_bind_result($stmt,$name,$mimeType);
+
+    /* fetch values */
+    while (mysqli_stmt_fetch($stmt)) {
+	
+		header("Content-type: " . mimeType);
+        echo $name;
+    }
+	
+	
+    mysqli_stmt_close($stmt);
+ 	
+}
+	
+
+  
+  
+ switch ($_GET['f']) {
+    case 'regi':
+       get_homonaiDaTa();
+        break;
+    
+	       // Do something
+
+ case 'file':
+       get_file();
+        break;
+    
+	}     
+
+
+
+?>
+<?php
+$conn->close();
+		
+?>
